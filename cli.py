25eecd3ed3bb9b893ce9a91ab6fed34da0714ebc
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2024-04-24 19:49:48 +1000

    click functions

---
+++ cli.py
@@ -0,0 +1,21 @@
+import click
+
+@click.command()
+def vocabulary():
+    # Vocabulary learning logic
+    pass
+
+@click.command()
+def grammar():
+    # Grammar exploration logic
+    pass
+
+@click.command()
+def exercises():
+    # Exercise practice logic
+    pass
+
+@click.command()
+def progress():
+    # Progress checking logic
+    pass
